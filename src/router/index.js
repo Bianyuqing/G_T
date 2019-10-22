727129f6534dce4d12815408662c5d964132bceb
bianyuqing <bianyuqing@example.com>
2019-10-22 20:49:53 +0800

    footer

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,41 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import VueRouter from 'vue-router'
+import Home from "@/components/all/home"
+import Classify from "@/components/all/classify"
+import Cart from "@/components/all/cart"
+import My from "@/components/all/my"
 
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+const routes=[
+  //  {
+    // path:'/',
+    // redirect:'/home' 
+  //  },
+  //  {
+  //   path:'/footer',
+  //   component:Footer
+  //  },
+   {
+     path:'/home',
+     component:Home,
+   },
+   {
+    path:'/classify',
+    component:Classify,
+  },
+  {
+    path:'/cart',
+    component:Cart,
+  },
+  {
+    path:'/my',
+    component:My,
+  }
+]
+const router = new VueRouter({
+    routes,
+    linkActiveClass:"red",
+});
+export default router;
