eab8eaa541ce3d0c036625e2fc110d2a33ff74bd
卫达 <1569528704@qq.com>
2019-10-22 20:57:33 +0800

    wei-ti

---
+++ src/pase/classfity/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        
+        <ul>
+            
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    created() {
+        this.$http.get("https://www.easy-mock.com/mock/5daef170374f867d204a19fc/classfity/fen").then(
+            res=>{
+                console.log(res)
+            }
+        )
+    },
+}
+</script>
+<style lang="scss">
+
+    
+</style>
